3bcd4ca137a99de6b5a77889d7f0892324ee64e1
ada <tadja8697@gmail.com>
2026-08-06 01:05:02 +0100

    Initialisation du backend

---
+++ src/main/java/com/kmermarket/kmerMarket/KmerMarketApplication.java
@@ -0,0 +1,13 @@
+package com.kmermarket.kmerMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmerMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmerMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kmermarket/kmerMarket/KmerMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kmermarket.kmerMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmerMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
